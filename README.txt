2eb84fa7e5bb95c8802884c0e69b68d36bd46673
LeslieK <leslieklein@comcast.net>
2014-01-06 17:42:01 -0500

    improved formatting of .py script

--- README.txt
+++ README.txt
@@ -1,28 +1,30 @@
 KnuthMorrisPratt.py: implementation of the KMP algorithm to search for a string (the pattern) in a text without backing up in the text.
 
-KeyIndexedCounting.py: 
+KeyIndexedCounting.py:
 implementation of radix sort algorithms
-implementation of a Suffix Array from an input string (aka text)
+implementation of a Suffix Array from an input string (aka 'the text')
 
 StringUtils.py:
 some general utility string functions
 
 Some Client scripts:
-LRS_script.py: 
-1 arg: filename: parses filename, reads in text, then finds lrs
+LRS_script.py:
+1 arg: filename: parses filename, reads in text, then finds longest repeated string (lrs)
 
 LongestRepeatedString.py:
 1 arg: filename: parses filename, reads in text; client can call functions on text; ie LRS(text)
 
 LRS_functions.py:
-a module of various functions; functions require that data is preprocessed into 
+a module of various functions; functions require that data is preprocessed into
 a sorted suffix array
 
 TwitterStrings.py:
-a script that needs your twitter credentials and 
-the twitter ids of people you follow and words/phrases that you track
+- a script that needs your twitter credentials and
+- the twitter ids of people you follow and
+- words/phrases that you track
 This script demos KNUTH-MORRIS-PRATT matching algorithm in a streaming feed
-update the script with your creds, enter your own pattern to search, and run the script!
+
+To run: update the script with your creds, enter your own pattern to search, and run the script!
 
 KMPExperiments.py
 calculates random number of compares for matching a random string in random text
--- TwitterStrings.py
+++ TwitterStrings.py
@@ -1,52 +1,48 @@
 from twitter import TwitterStream, OAuth
 from KnuthMorrisPratt import KMP
 
-CREDS = {
-	"consumer_key": "",
-	"consumer_secret": "",
-	"token": "",
-	"token_secret": ""
-}
+########################################################
+if __name__ == "__main__":
+    CREDS = {
+        "consumer_key": "fill-in-your-key",
+        "consumer_secret": "fill-in-your-secret",
+        "token": "fill-in-your-token",
+        "token_secret": "fill-in-your-token-secret"
+    }
 
-auth = OAuth(
-	consumer_key = CREDS["consumer_key"],
-	consumer_secret = CREDS["consumer_secret"],
-	token = CREDS["token"],
-	token_secret = CREDS["token_secret"]
-)
+    auth = OAuth(
+        consumer_key=CREDS["consumer_key"],
+        consumer_secret=CREDS["consumer_secret"],
+        token=CREDS["token"],
+        token_secret=CREDS["token_secret"]
+    )
 
-pat = "attack at dawn"
-kmp = KMP(pat)
-pattern_index = 0
+    pat = "weapons"
+    kmp = KMP(pat)
+    pattern_index = 0
 
-id_dict = {"wikileaks": "16589206", "aljazeera": "76067316", "NYFBI": "211635204", "reutersiran": "47633485", "WhiteHouse":"30313925"}
-public_stream = TwitterStream(auth = auth, domain='stream.twitter.com')
-iterator = public_stream.statuses.filter(language="en", follow=','.join(id_dict.values()), track="terrorism, weapons, drone attack, i want to kill")
-for item in iterator:
-	if item['retweeted'] == False:
-		try:
-			#print (''.join((item['user']['screen_name'], ':', item['text'])))
-			text = item['text']
-			print text
-			for c in text.encode():
-				#print c
-				pattern_index = kmp.dfa.next(c, pattern_index)
-				if int(pattern_index) == len(pat):
-					print "MATCH!!!!!"
-		except:
-			continue
+    id_dict = {
+        "wikileaks": "16589206",
+        "aljazeera": "76067316",
+        "NYFBI": "211635204",
+        "reutersiran": "47633485",
+        "WhiteHouse": "30313925"
+    }
 
-
-
-def charsfromfile(f):
-	'''a generator for reading a buffered file'''
-	while True:
-		a = array.array('c')
-		a.fromstring(f.read(1000))
-		if not a:
-			break
-		for x in a:
-			yield x
-
-
-	
+    public_stream = TwitterStream(auth=auth, domain='stream.twitter.com')
+    iterator = public_stream.statuses.filter(language="en",
+                                             follow=','.join(id_dict.values()),
+                                             track="terrorism, weapons, \
+                                             drone attack, i wantill")
+    for item in iterator:
+        if item['retweeted'] is False:
+            try:
+                text = item['text']
+                print text
+                for c in text.encode():
+                    #print c
+                    pattern_index = kmp.dfa.next(c, pattern_index)
+                    if int(pattern_index) == len(pat):
+                        print "MATCH!!!!!"
+            except:
+                continue
